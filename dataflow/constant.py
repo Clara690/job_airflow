3400bd8e9bad3f27fd0d1448354b5bf09acf6268
Clara690 <clara80416@gmail.com>
2026-08-28 21:57:20 +0200

    Fixed bugs and config setting

--- dataflow/constant.py
+++ dataflow/constant.py
@@ -12,8 +12,6 @@ DEFAULT_ARGS = {
     "start_date": datetime.datetime(2026, 7, 23),
     # 單一 task 最長可執行 60 分鐘，超時則視為失敗
     "execution_timeout": datetime.timedelta(minutes=60),
-    # manimum 10 task for DAG
-    "concurrency": 10,
 }
 
 # 限制此 DAG 同時最多只能執行一個 run（防止排程或手動重疊執行）
